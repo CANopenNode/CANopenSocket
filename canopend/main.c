45f0cf173497cfcbaf0f8d09194a33a8a217860f
Janez <janez.paternoster@siol.net>
2015-12-02 13:46:15 +0100

    Object Dictionary storage implemented.

--- canopend/main.c
+++ canopend/main.c
@@ -25,24 +25,18 @@
 
 
 #include "CANopen.h"
-#include "eeprom.h"
-#include "Linux_tasks.h"
+#include "CO_OD_storage.h"
+#include "CO_Linux_tasks.h"
 //#include "CgiLog.h"
 //#include "CgiOD.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <time.h>
 #include <signal.h>
 #include <errno.h>
-#include <fcntl.h>
-#include <sched.h>
-#include <sys/timerfd.h>
 #include <sys/epoll.h>
 #include <net/if.h>
-#include <sys/socket.h>
-#include <linux/can.h>
 #include <linux/reboot.h>
 #include <sys/reboot.h>
 
@@ -59,17 +53,20 @@
 
 
 /* Global variables and objects */
-    volatile uint16_t       CO_timer1ms = 0U;   /* variable increments each millisecond */
-    static int              rtPriority = -1;    /* Real time priority, configurable by arguments. (-1=RT disabled) */
-    static int              mainline_epoll_fd;  /* epoll file descriptor for mainline */
-    CO_EE_t                 CO_EEO;             /* EEprom object */
+volatile uint16_t           CO_timer1ms = 0U;   /* variable increments each millisecond */
+static int                  rtPriority = -1;    /* Real time priority, configurable by arguments. (-1=RT disabled) */
+static int                  mainline_epoll_fd;  /* epoll file descriptor for mainline */
+static CO_OD_storage_t      odStor;             /* Object Dictionary storage object for CO_OD_ROM */
+static CO_OD_storage_t      odStorAuto;         /* Object Dictionary storage object for CO_OD_EEPROM */
+static char                *odStorFile_rom    = "od_storage";       /* Name of the file */
+static char                *odStorFile_eeprom = "od_storage_auto";  /* Name of the file */
 
 
 /* Realtime thread */
 #ifndef CO_SINGLE_THREAD
-    static void*            rt_thread(void* arg);
-    static pthread_t        rt_thread_id;
-    static int              rt_thread_epoll_fd;
+static void*                rt_thread(void* arg);
+static pthread_t            rt_thread_id;
+static int                  rt_thread_epoll_fd;
 #endif
 
 
@@ -88,19 +85,19 @@
 
 
 /* Signal handler */
-    static volatile sig_atomic_t endProgram = 0;
-    static void sigHandler(int sig){
-        endProgram = 1;
-    }
+static volatile sig_atomic_t endProgram = 0;
+static void sigHandler(int sig) {
+    endProgram = 1;
+}
 
 
 /* Helper functions ***********************************************************/
-void CO_errExit(char* msg){
+void CO_errExit(char* msg) {
     perror(msg);
     exit(EXIT_FAILURE);
 }
 
-static void usageExit(char *progName){
+static void usageExit(char *progName) {
     fprintf(stderr, "Usage: %s <device> -i <Node ID (1..127)> [options]\n", progName);
     fprintf(stderr, "Options:\n");
     fprintf(stderr, "  -p <RT priority>    Realtime priority of RT task (RT disabled by default).\n");
@@ -113,9 +110,9 @@ static void usageExit(char *progName){
 /******************************************************************************/
 /** Mainline and RT thread                                                   **/
 /******************************************************************************/
-int main (int argc, char *argv[]){
+int main (int argc, char *argv[]) {
     CO_NMT_reset_cmd_t reset = CO_RESET_NOT;
-    CO_ReturnError_t eeStatus;
+    CO_ReturnError_t odStorStatus_rom, odStorStatus_eeprom;
     uint8_t powerOn = 1;
     int CANdevice0Index = 0;
     int opt;
@@ -138,23 +135,23 @@ int main (int argc, char *argv[]){
         }
     }
 
-    if(optind < argc){
+    if(optind < argc) {
         CANdevice = argv[optind];
         CANdevice0Index = if_nametoindex(CANdevice);
     }
 
-    if(nodeId < 1 || nodeId > 127){
+    if(nodeId < 1 || nodeId > 127) {
         fprintf(stderr, "Wrong node ID (%d)\n", nodeId);
         usageExit(argv[0]);
     }
 
     if(rtPriority != -1 && (rtPriority < sched_get_priority_min(SCHED_FIFO)
-                         || rtPriority > sched_get_priority_max(SCHED_FIFO))){
+                         || rtPriority > sched_get_priority_max(SCHED_FIFO))) {
         fprintf(stderr, "Wrong RT priority (%d)\n", rtPriority);
         usageExit(argv[0]);
     }
 
-    if(CANdevice0Index == 0){
+    if(CANdevice0Index == 0) {
         char s[120];
         snprintf(s, 120, "Can't find CAN device \"%s\"", CANdevice);
         CO_errExit(s);
@@ -179,35 +176,9 @@ int main (int argc, char *argv[]){
     }
 
 
-//    /* initialize battery powered SRAM */
-//    struct{
-//        uint8_t* EEPROMptr;
-//        uint32_t EEPROMsize;
-//        uint8_t* emcyBufPtr;
-//        uint32_t emcyBufSize;
-//        uint32_t size;
-//    }SRAM;
-//
-//    SRAM.size = 0;
-//    /* Sizes must be divisible by 4. (struct should have uint32_t as last element.) */
-//    SRAM.EEPROMsize = sizeof(CO_OD_EEPROM);                   SRAM.size += SRAM.EEPROMsize;
-//    SRAM.emcyBufSize = OD_CANopenLog.emcySRAMSize&0xFFFFFFFC; SRAM.size += SRAM.emcyBufSize;
-//
-//    if(RTX_MemWindow(gSysPublicData.sramAddr, SRAM.size)){
-//        SRAM.EEPROMptr = 0;
-//        SRAM.emcyBufPtr = 0;
-//        printf("\nError: SRAM initialization failed.");
-//        return 1;
-//    }
-//    else{
-//        SRAM.EEPROMptr = gSysPublicData.sramAddr;
-//        SRAM.emcyBufPtr = SRAM.EEPROMptr + SRAM.EEPROMsize;
-//    }
-
-
-    /* initialize EEPROM - part 1 */
-    eeStatus = CO_EE_init_1(&CO_EEO, NULL, (uint8_t*) &CO_OD_EEPROM, sizeof(CO_OD_EEPROM),
-                            (uint8_t*) &CO_OD_ROM, sizeof(CO_OD_ROM));
+    /* initialize Object Dictionary storage */
+    odStorStatus_rom = CO_OD_storage_init(&odStor, (uint8_t*) &CO_OD_ROM, sizeof(CO_OD_ROM), odStorFile_rom);
+    odStorStatus_eeprom = CO_OD_storage_init(&odStorAuto, (uint8_t*) &CO_OD_EEPROM, sizeof(CO_OD_EEPROM), odStorFile_eeprom);
 
 
 //    /* initialize CGI interface. These functions are bound with a fixed name and */
@@ -229,18 +200,18 @@ int main (int argc, char *argv[]){
     if(signal(SIGTERM, sigHandler) == SIG_ERR)
         CO_errExit("Program init - SIGTERM handler creation failed");
 
-    /* increase variable each startup. Variable is stored in eeprom. */
+    /* increase variable each startup. Variable is automatically stored in non-volatile memory. */
     OD_powerOnCounter++;
 
 
-    while(reset != CO_RESET_APP && reset != CO_RESET_QUIT && endProgram == 0){
+    while(reset != CO_RESET_APP && reset != CO_RESET_QUIT && endProgram == 0) {
 /* CANopen communication reset - initialize CANopen objects *******************/
         CO_ReturnError_t err;
 
         printf("%s - communication reset ...\n", argv[0]);
 
         /* Wait rt_thread, then configure CAN */
-        if(!firstRun){
+        if(!firstRun) {
             CO_LOCK_OD();
             CO->CANmodule[0]->CANnormal = false;
             CO_UNLOCK_OD();
@@ -251,19 +222,26 @@ int main (int argc, char *argv[]){
 
         /* initialize CANopen */
         err = CO_init(CANdevice0Index, nodeId, 0);
-        if(err != CO_ERROR_NO){
+        if(err != CO_ERROR_NO) {
             char s[120];
             snprintf(s, 120, "Communication reset - CANopen initialization failed, err=%d", err);
             CO_errExit(s);
         }
 
 
-        /* initialize eeprom - part 2 */
-        CO_EE_init_2(&CO_EEO, eeStatus, CO->SDO, CO->em);
+        /* initialize OD objects 1010 and 1011 and verify errors. */
+        CO_OD_configure(CO->SDO, OD_H1010_STORE_PARAM_FUNC, CO_ODF_1010, (void*)&odStor, 0, 0U);
+        CO_OD_configure(CO->SDO, OD_H1011_REST_PARAM_FUNC, CO_ODF_1011, (void*)&odStor, 0, 0U);
+        if(odStorStatus_rom != CO_ERROR_NO) {
+            CO_errorReport(CO->em, CO_EM_NON_VOLATILE_MEMORY, CO_EMC_HARDWARE, (uint32_t)odStorStatus_rom);
+        }
+        if(odStorStatus_eeprom != CO_ERROR_NO) {
+            CO_errorReport(CO->em, CO_EM_NON_VOLATILE_MEMORY, CO_EMC_HARDWARE, (uint32_t)odStorStatus_eeprom + 1000);
+        }
 
 
         /* just for info */
-        if(powerOn){
+        if(powerOn) {
             CO_errorReport(CO->em, CO_EM_MICROCONTROLLER_RESET, CO_EMC_NO_ERROR, OD_powerOnCounter);
             powerOn = 0;
         }
@@ -278,7 +256,7 @@ int main (int argc, char *argv[]){
 
 
         /* First time only initialization. */
-        if(firstRun){
+        if(firstRun) {
             firstRun = false;
 
             /* Configure epoll for mainline */
@@ -297,7 +275,7 @@ int main (int argc, char *argv[]){
             OD_performance[ODA_performance_timerCycleTime] = TMR_TASK_INTERVAL_NS/1000; /* informative */
 
             /* Set priority for mainline */
-            if(rtPriority > 0){
+            if(rtPriority > 0) {
                 struct sched_param param;
 
                 param.sched_priority = rtPriority;
@@ -320,7 +298,7 @@ int main (int argc, char *argv[]){
                 CO_errExit("Program init - rt_thread creation failed");
 
             /* Set priority for rt_thread */
-            if(rtPriority > 0){
+            if(rtPriority > 0) {
                 struct sched_param param;
 
                 param.sched_priority = rtPriority;
@@ -339,37 +317,37 @@ int main (int argc, char *argv[]){
         printf("%s - running ...\n", argv[0]);
 
 
-        while(reset == CO_RESET_NOT && endProgram == 0){
+        while(reset == CO_RESET_NOT && endProgram == 0) {
 /* loop for normal program execution ******************************************/
             int ready;
             struct epoll_event ev;
 
             ready = epoll_wait(mainline_epoll_fd, &ev, 1, -1);
 
-            if(ready != 1){
-                if(errno != EINTR){
+            if(ready != 1) {
+                if(errno != EINTR) {
                     CO_errorReport(CO->em, CO_EM_GENERIC_SOFTWARE_ERROR, CO_EMC_SOFTWARE_INTERNAL, 0x11100000L | errno);
                 }
             }
 
 #ifdef CO_SINGLE_THREAD
-            else if(CANrx_taskTmr_process(ev.data.fd)){
+            else if(CANrx_taskTmr_process(ev.data.fd)) {
                 /* code was processed in the above function. Additional code process below */
                 INCREMENT_1MS(CO_timer1ms);
                 /* Detect timer large overflow */
-                if(OD_performance[ODA_performance_timerCycleMaxTime] > TMR_TASK_OVERFLOW_US && rtPriority > 0){
+                if(OD_performance[ODA_performance_timerCycleMaxTime] > TMR_TASK_OVERFLOW_US && rtPriority > 0) {
                     CO_errorReport(CO->em, CO_EM_ISR_TIMER_OVERFLOW, CO_EMC_SOFTWARE_INTERNAL, 0x22400000L | OD_performance[ODA_performance_timerCycleMaxTime]);
                 }
             }
 #endif
 
-            else if(taskMain_process(ev.data.fd, &reset, CO_timer1ms)){
+            else if(taskMain_process(ev.data.fd, &reset, CO_timer1ms)) {
                 /* code was processed in the above function. Additional code process below */
                 //            CgiLogEmcyProcess(CgiLog);
-				CO_EE_process(&CO_EEO);
+                CO_OD_storage_autoSave(&odStorAuto, CO_timer1ms, 60000);
 #if 0
 				static uint16_t maxIntervalMain = 0;
-				if(maxIntervalMain < timer1msDiff){
+				if(maxIntervalMain < timer1msDiff) {
 					maxIntervalMain = timer1msDiff;
 					printf("Maximum main interval time: %d milliseconds\n", maxIntervalMain);
 				}
@@ -377,7 +355,7 @@ int main (int argc, char *argv[]){
 #endif
             }
 
-            else{
+            else {
                 /* No file descriptor was processed. */
                 CO_errorReport(CO->em, CO_EM_GENERIC_SOFTWARE_ERROR, CO_EMC_SOFTWARE_INTERNAL, 0x11200000L);
             }
@@ -389,10 +367,14 @@ int main (int argc, char *argv[]){
     /* join threads */
     endProgram = 1;
 #ifndef CO_SINGLE_THREAD
-    if(pthread_join(rt_thread_id, NULL) != 0){
+    if(pthread_join(rt_thread_id, NULL) != 0) {
         CO_errExit("Program end - pthread_join failed");
     }
 #endif
+
+    /* Store CO_OD_EEPROM */
+    CO_OD_storage_autoSave(&odStorAuto, 0, 0);
+    CO_OD_storage_autoSaveClose(&odStorAuto);
 
     /* delete objects from memory */
     CANrx_taskTmr_close();
@@ -402,9 +384,9 @@ int main (int argc, char *argv[]){
     printf("%s on %s (nodeId=0x%02X) - finished.\n\n", argv[0], CANdevice, nodeId);
 
     /* Flush all buffers (and reboot) */
-    if(rebootEnable && reset == CO_RESET_APP){
+    if(rebootEnable && reset == CO_RESET_APP) {
         sync();
-        if(reboot(LINUX_REBOOT_CMD_RESTART) != 0){
+        if(reboot(LINUX_REBOOT_CMD_RESTART) != 0) {
             CO_errExit("Program end - reboot failed");
         }
     }
@@ -415,43 +397,43 @@ int main (int argc, char *argv[]){
 
 #ifndef CO_SINGLE_THREAD
 /* Realtime thread for CAN receive and taskTmr ********************************/
-static void* rt_thread(void* arg){
+static void* rt_thread(void* arg) {
 
     /* Endless loop */
-    while(endProgram == 0){
+    while(endProgram == 0) {
         int ready;
         struct epoll_event ev;
 
         ready = epoll_wait(rt_thread_epoll_fd, &ev, 1, -1);
 
-        if(ready != 1){
-            if(errno != EINTR){
+        if(ready != 1) {
+            if(errno != EINTR) {
                 CO_errorReport(CO->em, CO_EM_GENERIC_SOFTWARE_ERROR, CO_EMC_SOFTWARE_INTERNAL, 0x12100000L | errno);
             }
         }
 
-        else if(CANrx_taskTmr_process(ev.data.fd)){
+        else if(CANrx_taskTmr_process(ev.data.fd)) {
             /* code was processed in the above function. Additional code process below */
             INCREMENT_1MS(CO_timer1ms);
             /* Detect timer large overflow */
-            if(OD_performance[ODA_performance_timerCycleMaxTime] > TMR_TASK_OVERFLOW_US && rtPriority > 0){
+            if(OD_performance[ODA_performance_timerCycleMaxTime] > TMR_TASK_OVERFLOW_US && rtPriority > 0) {
                 CO_errorReport(CO->em, CO_EM_ISR_TIMER_OVERFLOW, CO_EMC_SOFTWARE_INTERNAL, 0x22400000L | OD_performance[ODA_performance_timerCycleMaxTime]);
             }
 #if 0
 			static uint16_t maxInterval = 0;
 			static uint16_t cntxx = 0;
-			if(maxInterval < OD_performance[ODA_performance_timerCycleMaxTime]){
+			if(maxInterval < OD_performance[ODA_performance_timerCycleMaxTime]) {
 				maxInterval = OD_performance[ODA_performance_timerCycleMaxTime];
 				printf("Maximum timer interval time: %5d microseconds\n", maxInterval);
 			}
-			if(++cntxx == 1000){
+			if(++cntxx == 1000) {
 				cntxx = 0;
 				printf("\n");
 			}
 #endif
         }
 
-        else{
+        else {
             /* No file descriptor was processed. */
             CO_errorReport(CO->em, CO_EM_GENERIC_SOFTWARE_ERROR, CO_EMC_SOFTWARE_INTERNAL, 0x12200000L);
         }
